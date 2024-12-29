c55cdd9331db33ced182851f1b096c48912274fd
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2024-12-29 14:23:08 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import heroine from './heroine.jpg';
+</script>
+
+<img src={heroine} alt="Ela, Lena und Rika" class="mb-20" />
+
+<div class="mb-20 text-center">
+	<h1 class="mb-4 text-5xl lg:text-8xl">Raphaela & Lena</h1>
+	<p class="text-3xl lg:text-6xl">20.09.2025</p>
+</div>
+
+<div class="p-6 text-center text-3xl lg:text-6xl">
+	<p class="mb-8">
+		Du hast uns auf unserem Weg begleitet und bist uns wichtig. Wir laden Dich herzlich dazu ein,
+		unsere Liebe mit uns zu feiern.
+	</p>
+	<p class="mb-8">Am 20.9. 2025 heiraten wir auf dem Lebenshof "MutimBauch" im Schwarzwald.</p>
+	<p>
+		Erscheine so wie Du Dich wohlfühlst. Sei einfach Du selbst, mit all Deinen Farben und dem was
+		Dich ausmacht.
+	</p>
+</div>
